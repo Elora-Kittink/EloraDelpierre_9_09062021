9531d13fc0f18e20e6a5c6c686012114a19aec96
Elora <elora.delpierre@gmail.com>
2021-06-14 12:01:04 +0200

    resolution format img

--- src/app/format.js
+++ src/app/format.js
@@ -1,11 +1,14 @@
 export const formatDate = (dateStr) => {
+  console.log(dateStr);
   const date = new Date(dateStr);
+  console.log(date);
   const ye = new Intl.DateTimeFormat("fr", { year: "numeric" }).format(date);
-  const mo = new Intl.DateTimeFormat("fr", { month: "short" }).format(date);
+  const mo = new Intl.DateTimeFormat("fr", { month: "2-digit" }).format(date);
   const da = new Intl.DateTimeFormat("fr", { day: "2-digit" }).format(date);
   const month = mo.charAt(0).toUpperCase() + mo.slice(1);
   // return `${parseInt(da)} ${month.substr(0, 3)}. ${ye.toString().substr(2, 4)}`;
-  return `${ye.toString().substr(2, 4)}. ${month.substr(0, 3)}.${parseInt(da)}`;
+  console.log(`${ye}.${month}.${da}`);
+  return `${ye}.${month}.${da}`;
 }; // retourne un format jour/mois/année alors que le test attends un format année/mois/jour//
 
 export const formatStatus = (status) => {
